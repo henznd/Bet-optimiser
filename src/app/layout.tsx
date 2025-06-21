facfe298d052a4f0dea5ddcf8625b5414afe3e13
Baptiste Cuchet <baptistecuchet@MacBook-Air-de-Baptiste.local>
2025-06-21 11:03:06 +0200

    style: Set light theme as default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="fr" className="dark" suppressHydrationWarning>
+    <html lang="fr" suppressHydrationWarning>
       <body className={inter.className}>{children}</body>
     </html>
   )
